9d464dfc577b423392227c9112aa290b79aafb9e
s-fukuzawaa <31524675+s-fukuzawaa@users.noreply.github.com>
2019-04-17 07:58:14 -0700

    Working on Lab 4 now

---
+++ Lab4/Lab4/Financial.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Lab4
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Financial : Page
+    {
+        public Financial()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(ShareListBoxItem.IsSelected)
+            {
+                ResultTextBlock.Text = "Share";
+            }
+            else if(FavoritesListBoxItem.IsSelected)
+            {
+                ResultTextBlock.Text = "Favorites";
+            }
+
+        }
+    }
+}
--- Lab4/Lab4/Food.xaml.cs
+++ Lab4/Lab4/Food.xaml.cs
@@ -13,23 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Lab4
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Food : Page
     {
-        public MainPage()
+        public Food()
         {
             this.InitializeComponent();
-        }
-
-        private void Main_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(Financial));
         }
     }
 }
