5479fc9fd0e33c2db077025184c70d405a70e2e0
s-fukuzawaa <31524675+s-fukuzawaa@users.noreply.github.com>
2019-04-26 09:37:08 -0700

    Data binding slides started.

---
+++ Lecture/Lecture/BookManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    public class BookManager
+    {
+        public static List<Book> GetBooks()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book { BookId = 1, Title = "Vulpate", Author = "Futurum", CoverImage = "Assets/1.jpg" });
+            books.Add(new Book { BookId = 2, Title = "Mazim", Author = "Sequiter Que", CoverImage = "Assets/2.jpg" });
+            books.Add(new Book { BookId = 3, Title = "Elit", Author = "Tempor", CoverImage = "Assets/3.jpg" });
+
+            return books;
+        }
+    }
+}
