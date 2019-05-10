2fb602712748f82ed10952367e472cbcc5513c73
s-fukuzawaa <31524675+s-fukuzawaa@users.noreply.github.com>
2019-05-10 13:27:18 -0700

    In the process of doing data binding. having red squiggles. saving just in case.

---
+++ IndependentProject/IndependentProject/ViewModels/WelcomePageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+using System.ComponentModel;
+
+namespace IndependentProject.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class WelcomePageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ImageUrl { get; set; }
+    }
+}
